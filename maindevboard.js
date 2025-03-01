b8befb4d2116439c7f3b04159c547ad3274aa03d
NahidSarkar7 <spaceavoid46@gmail.com>
2025-03-01 23:28:15 +0600

    My awesome devBoard

---
+++ maindevboard.js
@@ -0,0 +1,453 @@
+ 
+
+//complete button functionality
+
+const CompletedBtn = document.getElementById('Completed1');
+
+CompletedBtn.addEventListener('click',function(event){
+
+    event.preventDefault();
+    alert('Board updated successfully');
+
+    const CompletedBtn = document.getElementById('Completed1');
+
+    let taskNumber = document.getElementById('taskNumber').innerText;
+    let increaseNUmber = document.getElementById('increasNumber').innerText;
+    console.log(taskNumber);
+    console.log(increaseNUmber);
+
+    taskValue = taskNumber-1;
+    console.log(taskValue);
+
+    document.getElementById('taskNumber').innerText=taskValue;
+    
+    inceasValue = parseInt(increaseNUmber)+1;
+    console.log(typeof increaseNUmber)
+
+    document.getElementById('increasNumber').innerText=inceasValue;
+
+    CompletedBtn.setAttribute('nameOfAttribute' ,'ValueOf Attribute');
+    CompletedBtn.setAttribute('disabled' ,' ');
+    console.log(CompletedBtn);
+
+const containValue = document.getElementById('father-of-container');
+
+
+const d = new Date();
+
+const time= d.toLocaleTimeString();
+
+const card = document.getElementById('card-1').innerText;
+
+const p =document.createElement('p');
+p.innerText=`
+You have completed the ${card} at ${time}
+`
+
+p.style.backgroundColor='#EEF3FF';
+p.style.fontFamily='poppins';
+p.style.borderRadius='10px';
+p.style.padding='10px';
+p.style.width='375px';
+p.style.margin='auto';
+p.style.marginTop='10px';
+
+
+containValue.appendChild(p);
+
+console.log( p.innerText)
+
+    
+});
+
+
+
+
+
+//complete button functionality
+
+const CompletedBtn2 = document.getElementById('Completed2');
+
+CompletedBtn2.addEventListener('click',function(event){
+
+    event.preventDefault();
+    alert('Board updated successfully');
+
+    const CompletedBtn2 = document.getElementById('Completed2');
+
+    let taskNumber = document.getElementById('taskNumber').innerText;
+    let increaseNUmber = document.getElementById('increasNumber').innerText;
+    console.log(taskNumber);
+    console.log(increaseNUmber);
+
+    taskValue = taskNumber-1;
+    console.log(taskValue);
+
+    document.getElementById('taskNumber').innerText=taskValue;
+    
+    inceasValue = parseInt(increaseNUmber)+1;
+    console.log(typeof increaseNUmber)
+
+    document.getElementById('increasNumber').innerText=inceasValue;
+
+    CompletedBtn.setAttribute('nameOfAttribute' ,'ValueOf Attribute');
+    CompletedBtn2.setAttribute('disabled' ,' ');
+    console.log(CompletedBtn);
+
+const containValue = document.getElementById('father-of-container');
+
+
+const d = new Date();
+
+const time= d.toLocaleTimeString();
+
+const card = document.getElementById('card-2').innerText;
+
+const p =document.createElement('p');
+p.innerText=`
+You have completed the ${card} at ${time}
+`
+
+p.style.backgroundColor='#EEF3FF';
+p.style.fontFamily='poppins';
+p.style.borderRadius='10px';
+p.style.padding='10px';
+p.style.width='375px';
+p.style.margin='auto';
+p.style.marginTop='10px';
+
+
+containValue.appendChild(p);
+
+console.log( p.innerText)
+
+    
+});
+
+
+
+
+//complete button functionality
+
+const CompletedBtn3 = document.getElementById('Completed3');
+
+CompletedBtn3.addEventListener('click',function(event){
+
+    event.preventDefault();
+    alert('Board updated successfully');
+
+    const CompletedBtn3 = document.getElementById('Completed3');
+
+    let taskNumber = document.getElementById('taskNumber').innerText;
+    let increaseNUmber = document.getElementById('increasNumber').innerText;
+    console.log(taskNumber);
+    console.log(increaseNUmber);
+
+    taskValue = taskNumber-1;
+    console.log(taskValue);
+
+    document.getElementById('taskNumber').innerText=taskValue;
+    
+    inceasValue = parseInt(increaseNUmber)+1;
+    console.log(typeof increaseNUmber)
+
+    document.getElementById('increasNumber').innerText=inceasValue;
+
+    CompletedBtn.setAttribute('nameOfAttribute' ,'ValueOf Attribute');
+    CompletedBtn3.setAttribute('disabled' ,' ');
+    console.log(CompletedBtn);
+
+const containValue = document.getElementById('father-of-container');
+
+
+
+const d = new Date();
+
+const time= d.toLocaleTimeString();
+
+const card = document.getElementById('card-3').innerText;
+
+const p =document.createElement('p');
+p.innerText=`
+You have completed the ${card} at ${time}
+`
+
+p.style.backgroundColor='#EEF3FF';
+p.style.fontFamily='poppins';
+p.style.borderRadius='10px';
+p.style.padding='10px';
+p.style.width='375px';
+p.style.margin='auto';
+p.style.marginTop='10px';
+
+
+containValue.appendChild(p);
+
+console.log( p.innerText)
+
+    
+});
+
+
+
+//complete button functionality
+
+const CompletedBtn4 = document.getElementById('Completed4');
+
+CompletedBtn4.addEventListener('click',function(event){
+
+    event.preventDefault();
+    alert('Board updated successfully');
+
+    const CompletedBtn4 = document.getElementById('Completed4');
+
+    let taskNumber = document.getElementById('taskNumber').innerText;
+    let increaseNUmber = document.getElementById('increasNumber').innerText;
+    console.log(taskNumber);
+    console.log(increaseNUmber);
+
+    taskValue = taskNumber-1;
+    console.log(taskValue);
+
+    document.getElementById('taskNumber').innerText=taskValue;
+    
+    inceasValue = parseInt(increaseNUmber)+1;
+    console.log(typeof increaseNUmber)
+
+    document.getElementById('increasNumber').innerText=inceasValue;
+
+    CompletedBtn.setAttribute('nameOfAttribute' ,'ValueOf Attribute');
+    CompletedBtn4.setAttribute('disabled' ,' ');
+    console.log(CompletedBtn);
+
+const containValue = document.getElementById('father-of-container');
+
+
+const d = new Date();
+
+const time= d.toLocaleTimeString();
+
+const card = document.getElementById('card-4').innerText;
+
+const p =document.createElement('p');
+p.innerText=`
+You have completed the ${card} at ${time}
+`
+
+p.style.backgroundColor='#EEF3FF';
+p.style.fontFamily='poppins';
+p.style.borderRadius='10px';
+p.style.padding='10px';
+p.style.width='375px';
+p.style.margin='auto';
+p.style.marginTop='10px';
+
+
+containValue.appendChild(p);
+
+console.log( p.innerText)
+
+    
+});
+
+
+
+//complete button functionality
+
+const CompletedBtn5 = document.getElementById('Completed5');
+
+CompletedBtn5.addEventListener('click',function(event){
+
+    event.preventDefault();
+    alert('Board updated successfully');
+
+    const CompletedBtn5 = document.getElementById('Completed5');
+
+    let taskNumber = document.getElementById('taskNumber').innerText;
+    let increaseNUmber = document.getElementById('increasNumber').innerText;
+    console.log(taskNumber);
+    console.log(increaseNUmber);
+
+    taskValue = taskNumber-1;
+    console.log(taskValue);
+
+    document.getElementById('taskNumber').innerText=taskValue;
+    
+    inceasValue = parseInt(increaseNUmber)+1;
+    console.log(typeof increaseNUmber)
+
+    document.getElementById('increasNumber').innerText=inceasValue;
+
+    CompletedBtn.setAttribute('nameOfAttribute' ,'ValueOf Attribute');
+    CompletedBtn5.setAttribute('disabled' ,' ');
+    console.log(CompletedBtn);
+
+const containValue = document.getElementById('father-of-container');
+
+
+const d = new Date();
+
+const time= d.toLocaleTimeString();
+
+const card = document.getElementById('card-5').innerText;
+
+const p =document.createElement('p');
+p.innerText=`
+You have completed the ${card} at ${time}
+`
+
+p.style.backgroundColor='#EEF3FF';
+p.style.fontFamily='poppins';
+p.style.borderRadius='10px';
+p.style.padding='10px';
+p.style.width='375px';
+p.style.margin='auto';
+p.style.marginTop='10px';
+
+
+containValue.appendChild(p);
+
+
+console.log( p.innerText)
+
+    
+});
+
+
+
+
+//complete button functionality
+
+const CompletedBtn6 = document.getElementById('Completed6');
+
+CompletedBtn6.addEventListener('click',function(event){
+
+    event.preventDefault();
+    alert('Board updated successfully');
+
+    const CompletedBtn = document.getElementById('Completed6');
+
+    let taskNumber = document.getElementById('taskNumber').innerText;
+    let increaseNUmber = document.getElementById('increasNumber').innerText;
+    console.log(taskNumber);
+    console.log(increaseNUmber);
+
+    taskValue = taskNumber-1;
+    console.log(taskValue);
+
+    document.getElementById('taskNumber').innerText=taskValue;
+    
+    inceasValue = parseInt(increaseNUmber)+1;
+    console.log(typeof increaseNUmber)
+
+    document.getElementById('increasNumber').innerText=inceasValue;
+
+    CompletedBtn.setAttribute('nameOfAttribute' ,'ValueOf Attribute');
+    CompletedBtn6.setAttribute('disabled' ,' ');
+    console.log(CompletedBtn);
+
+const containValue = document.getElementById('father-of-container');
+
+
+const d = new Date();
+
+const time= d.toLocaleTimeString();
+
+
+const card = document.getElementById('card-6').innerText;
+
+const p =document.createElement('p');
+p.innerText=`
+You have completed the ${card} at ${time}
+`
+
+p.style.backgroundColor='#EEF3FF';
+p.style.fontFamily='poppins';
+p.style.borderRadius='10px';
+p.style.padding='10px';
+p.style.width='375px';
+p.style.margin='auto';
+p.style.marginTop='10px';
+
+
+containValue.appendChild(p);
+
+
+alert('Congratulation! you have completed all current TASK')
+
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById('clear').addEventListener('click',function(event){
+
+// alert('Hello')
+
+const containValue = document.getElementById('father-of-container');
+
+containValue.innerText=''
+
+
+})
+
+
+//Background change of body function
+
+document.getElementById('circle-image-btn').addEventListener('click',function(){
+
+let r = Math.floor(Math.random()*256);
+let g = Math.floor(Math.random()*256);
+let b = Math.floor(Math.random()*256);
+ 
+
+let BackgroundC = "rgb(" + r + ',' + g + ","+ b +")" ;
+console.log(BackgroundC);
+
+let body =document.getElementById('body');
+
+body.style.backgroundColor=BackgroundC;
+  
+
+})
+
+
+//showing the current date
+
+
+const date = new Date();
+
+const currentDate = date.toDateString();
+const value7 =currentDate.split(' ')
+console.log(value7);
+
+
+const mainDate =document.getElementById('week').innerText=value7[0];
+
+document.getElementById('month').innerText=value7[1];
+document.getElementById('day').innerText=value7[2];
+document.getElementById('year').innerText=value7[3];
+
+// const d = new Date();
+
+// const time= d.toLocaleTimeString();
+
+// document.getElementById('headi').innerText=time;
+
+//Next page script
+
+document.getElementById('discover').addEventListener('click',function(){
+    
+    window.location.href='anotherpage.html';
+
+});
